436ae4f5623bf38fc6a92e65cb301944391f61a6
gialluted <giallu.ted@gmail.com>
2025-10-31 15:01:00 +0100

    Refactor variable names and streamline predictions calculation; add rounding and MCC calculation

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import LeaveOneOut, cross_val_predict
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import mean_squared_error, r2_score, matthews_corrcoef
 
 data = np.genfromtxt('10_7717_peerj_5665_dataYM2018_neuroblastoma.csv', 
                      delimiter=',')
 data = data[~np.isnan(data).any(axis=1)]
-input = data[:, :-1]
-output = data[:, -1]
+variabili = data[:, :-1]
+outcome = data[:, -1]
 
 #np.set_printoptions(threshold=np.inf)
-#print(input)
-#print(output)
+#print(variabili)
+#print(outcome)
 
-predictions = cross_val_predict(LinearRegression(), input, output, cv=LeaveOneOut())
-#r2 = r2_score(output, predictions)
-#mse = mean_squared_error(output, predictions)
+predictions = cross_val_predict(LinearRegression(), variabili, outcome, cv=LeaveOneOut())
+
+#r2 = r2_score(outcome, predictions)
+#mse = mean_squared_error(outcome, predictions)
 #rmse = np.sqrt(mse)
-print(predictions)
-
-#mcc = matthews_corrcoef(output, predictions)
-#print(f"MCC: {mcc}")
-
 #print(f"R-squared (R2) medio: {r2}")
 #print(f"Errore Quadratico Medio (MSE): {mse}")
 #print(f"Radice dell'Errore Quadratico Medio (RMSE): {rmse}")
+
+for i in range(len(predictions)):
+    predictions[i] = round(predictions[i])
+
+#print(predictions)
+
+mcc = matthews_corrcoef(outcome, predictions)
+print(f"Coefficiente di Correlazione di Matthews (MCC): {mcc}")
