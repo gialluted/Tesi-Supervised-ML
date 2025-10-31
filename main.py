54431f7522d018cd502850939b78279be18f7c32
gialluted <giallu.ted@gmail.com>
2025-10-31 14:16:48 +0100

    Aggiungi importazione di matthews_corrcoef e commenta il codice di valutazione delle metriche

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import LeaveOneOut, cross_val_predict
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import matthews_corrcoef
 
 data = np.genfromtxt('10_7717_peerj_5665_dataYM2018_neuroblastoma.csv', 
                      delimiter=',')
@@ -14,10 +15,14 @@ output = data[:, -1]
 #print(output)
 
 predictions = cross_val_predict(LinearRegression(), input, output, cv=LeaveOneOut())
-r2 = r2_score(output, predictions)
-mse = mean_squared_error(output, predictions)
-rmse = np.sqrt(mse)
+#r2 = r2_score(output, predictions)
+#mse = mean_squared_error(output, predictions)
+#rmse = np.sqrt(mse)
+print(predictions)
 
-print(f"R-squared (R2) medio: {r2}")
-print(f"Errore Quadratico Medio (MSE): {mse}")
-print(f"Radice dell'Errore Quadratico Medio (RMSE): {rmse}")
+#mcc = matthews_corrcoef(output, predictions)
+#print(f"MCC: {mcc}")
+
+#print(f"R-squared (R2) medio: {r2}")
+#print(f"Errore Quadratico Medio (MSE): {mse}")
+#print(f"Radice dell'Errore Quadratico Medio (RMSE): {rmse}")
