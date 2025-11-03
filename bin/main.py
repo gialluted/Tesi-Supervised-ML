7c1ba4bd410c020977475d8879634291a15167db
gialluted <giallu.ted@gmail.com>
2025-11-03 15:01:38 +0100

    Refactor code for improved readability by removing unnecessary whitespace and comments

--- bin/main.py
+++ bin/main.py
@@ -1,14 +1,10 @@
 import numpy as np
-
 from sklearn.linear_model import LinearRegression
-
 from sklearn.model_selection import LeaveOneOut, cross_val_predict
-
 from sklearn.metrics import mean_squared_error, r2_score, matthews_corrcoef
 import time
 
 start = time.time()
-
 
 data = np.genfromtxt('data/10_7717_peerj_5665_dataYM2018_neuroblastoma.csv', 
                      delimiter=',')
@@ -17,27 +13,17 @@ variabili = data[:, :-1]
 outcome = data[:, -1]
 
 #np.set_printoptions(threshold=np.inf)
-
 #print(variabili)
-
 #print(outcome)
-
 
 predictions = cross_val_predict(LinearRegression(), variabili, outcome, cv=LeaveOneOut())
 
-
 #r2 = r2_score(outcome, predictions)
-
 #mse = mean_squared_error(outcome, predictions)
-
 #rmse = np.sqrt(mse)
-
 #print(f"R-squared (R2) medio: {r2}")
-
 #print(f"Errore Quadratico Medio (MSE): {mse}")
-
 #print(f"Radice dell'Errore Quadratico Medio (RMSE): {rmse}")
-
 
 for i in range(len(predictions)):
 
@@ -49,13 +35,9 @@ for i in range(len(predictions)):
 
         predictions[i] = 0
 
-
 #print(predictions)
 
-
 mcc = matthews_corrcoef(outcome, predictions)
-
 print(f"Coefficiente di Correlazione di Matthews (MCC): {mcc}")
-
 
 print("Durata dell'esecuzione del programma: %s secondi" % (time.time() - start))
