402113f9f765c3a440b0d722d8e9e79f647d036c
gialluted <giallu.ted@gmail.com>
2026-02-02 19:13:49 +0100

    R

--- bin/R/progetto.R
+++ bin/R/progetto.R
@@ -1,3 +1,6 @@
+library(caret)
+library(mltools)
+
 start_time <- Sys.time()
 
 data <- read.csv("GitHub/Tesi-Supervised-ML/data/10_7717_peerj_5665_dataYM2018_neuroblastoma.csv", header = TRUE)
@@ -6,9 +9,19 @@ data <- na.omit(data)
 X <- data[, -ncol(data)]
 y <- data[, ncol(data)]
 
-#print(X)
-#print(y)
+ctrl <- trainControl(method = "LOOCV", savePredictions = "final")
+
+model <- train(X, y, method = "lm", trControl = ctrl)
+
+predictions <- model$pred$pred
+
+y_pred_binary <- ifelse(predictions > 0.5, 1, 0)
+
+y_ordered <- y[model$pred$rowIndex]
+
+mcc <- mcc(preds = y_pred_binary, actuals = y)
+cat(sprintf("Coefficiente di Correlazione di Matthews (MCC): %.15f\n", mcc))
 
 end_time <- Sys.time()
 duration <- as.numeric(difftime(end_time, start_time, units = "secs"))
-cat(sprintf("Durata dell'esecuzione del programma: %.10f secondi\n", duration))
+cat(sprintf("Durata dell'esecuzione del programma: %.4f secondi\n", duration))
--- src/R/progetto.R
+++ src/R/progetto.R
@@ -1,4 +1,5 @@
 library(caret)
+library(mltools)
 
 start_time <- Sys.time()
 
@@ -8,39 +9,17 @@ data <- na.omit(data)
 X <- data[, -ncol(data)]
 y <- data[, ncol(data)]
 
-#print(X)
-#print(y)
-
-end_time <- Sys.time()
-duration <- as.numeric(difftime(end_time, start_time, units = "secs"))
-cat(sprintf("Durata dell'esecuzione del programma: %.10f secondi\n", duration))
-
-X <- data[, -ncol(data)]
-y <- data[, ncol(data)]
-
-ctrl <- trainControl(method = "LOOCV")
+ctrl <- trainControl(method = "LOOCV", savePredictions = "final")
 
 model <- train(X, y, method = "lm", trControl = ctrl)
 
-predictions <- predict(model, X)
+predictions <- model$pred$pred
 
 y_pred_binary <- ifelse(predictions > 0.5, 1, 0)
 
-calculate_mcc <- function(y_true, y_pred) {
-  TP <- sum(y_true == 1 & y_pred == 1)
-  TN <- sum(y_true == 0 & y_pred == 0)
-  FP <- sum(y_true == 0 & y_pred == 1)
-  FN <- sum(y_true == 1 & y_pred == 0)
-  
-  numerator <- (TP * TN) - (FP * FN)
-  denominator <- sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
-  
-  if (denominator == 0) return(0.0)
-  return(numerator / denominator)
-}
+y_ordered <- y[model$pred$rowIndex]
 
-mcc <- calculate_mcc(y, y_pred_binary)
-
+mcc <- mcc(preds = y_pred_binary, actuals = y)
 cat(sprintf("Coefficiente di Correlazione di Matthews (MCC): %.15f\n", mcc))
 
 end_time <- Sys.time()
